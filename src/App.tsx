3ac646a68cef2bfa3e249295fa5c9918f98a0ea6
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-05-02 19:00:38 +0100

    Fetched all countries data from their endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios"
 
+interface Countries {
+
+  name: {
+    common: string,
+    official: string
+  },
+  region: string,
+  population: string
+}
 function App() {
+
+  const [countriesData, setCountriesData] = useState<Countries[]>([])
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all")
+      .then(response => {
+        setCountriesData(response.data)
+      })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      lkjihugyt
+      {countriesData && countriesData.map((country) => (
+        <div key={country.name.official}>
+          <p>{country.name.official}</p>
+          <p>{country.region}</p>
+          <p>{country.population.toLocaleString()}</p>
+        </div>
+      ))}
     </div>
   );
 }
