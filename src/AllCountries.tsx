995fc5b6c0f76916fd3e494ee7a2bd1f5f9eb943
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-05-03 11:55:27 +0100

    Added search functionality for searching by country

--- src/AllCountries.tsx
+++ src/AllCountries.tsx
@@ -5,44 +5,62 @@ import { Link } from 'react-router-dom';
 
 interface Countries {
 
-  name: {
-    common: string,
-    official: string
-  },
-  flags: {
-    svg: string
-  },
-  region: string,
-  population: string
+    name: {
+        common: string,
+        official: string
+    },
+    flags: {
+        svg: string
+    },
+    region: string,
+    population: string
 }
 function AllCountries() {
 
-  const [countriesData, setCountriesData] = useState<Countries[]>([])
+    const [countriesData, setCountriesData] = useState<Countries[]>([])
+    const [searchCountry, setSearchCountry] = useState<string>("")
+    useEffect(() => {
+        axios.get("https://restcountries.com/v3.1/all")
+            .then(response => {
+                setCountriesData(response.data)
+            })
+    }, [])
 
-  useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all")
-      .then(response => {
-        setCountriesData(response.data)
-      })
-  })
+     function getCountry() {
+        axios.get(`https://restcountries.com/v3.1/name/${searchCountry}`)
+            .then(response => {
+                setCountriesData(response.data)
+            })
+    }
+    const handleForm = (event: any) => {
+        event.preventDefault();
+        getCountry();
+    }
 
-  return (
-    <div>
-
-      <div>
-        {countriesData && countriesData.map((country) => (
-          <Link to={`/${country.name.common}`}>
-            <div key={country.name.official}>
-              <img width={240} src={country.flags.svg} alt='flag' />
-              <p>{country.name.official}</p>
-              <p>{country.region}</p>
-              <p>{country.population.toLocaleString()}</p>
+    return (
+        <div>
+            <form onSubmit={handleForm}>
+                <input type='text'
+                    value={searchCountry}
+                    onChange={(event) => setSearchCountry(event.target.value)}
+                    name="search"
+                    id='search'
+                    placeholder='Search Country' />
+            </form>
+            <div>
+                {countriesData && countriesData.map((country) => (
+                    <Link to={`/${country.name.common}`} key={country.name.official}>
+                        <div>
+                            <img width={240} src={country.flags.svg} alt='flag' />
+                            <p>{country.name.official}</p>
+                            <p>{country.region}</p>
+                            <p>{country.population.toLocaleString()}</p>
+                        </div>
+                    </Link>
+                ))}
             </div>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default AllCountries;
--- src/IndividualCountry.tsx
+++ src/IndividualCountry.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+interface CountryData {
+    name: {
+        common: string,
+        official: string
+    },
+    flags: {
+        svg: string
+    },
+    capital: string[],
+    region: string,
+    population: string,
+    border: string[],
+    subregion: string[]
+}
 
 const IndividualCountry = () => {
-  return (
-    <div>IndividualCountry</div>
-  )
+    const [singleCountry, setSingleCountry] = useState<CountryData[]>([])
+    const { name } = useParams();
+
+    useEffect(() => {
+        axios.get(`https://restcountries.com/v3.1/name/${name}`)
+            .then(response => {
+                setSingleCountry(response.data)
+            })
+    }, [name])
+
+
+    return (
+        <div>
+            {
+                singleCountry && singleCountry.map((eachCountry) => (
+                    <div key={eachCountry.name.common}>
+                        <img width={240} src={eachCountry.flags.svg} alt='flag' />
+                        <p>{eachCountry.name.official}</p>
+                        <p>Capital: {eachCountry.capital[0]}</p>
+                        <p>Population: {eachCountry.population.toLocaleString()}</p>
+                        <p> Region: {eachCountry.region}</p>
+                        <p>Subregion: {eachCountry.subregion}</p>
+                    </div>
+
+                ))
+            }
+        </div>
+    )
 }
 
 export default IndividualCountry
