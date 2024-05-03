e27d1a0e3cc93cfa7457298c7651aea5ae6cb28e
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-05-03 12:11:15 +0100

    Added antd select/option component

--- src/AllCountries.tsx
+++ src/AllCountries.tsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from "axios"
 import { Link } from 'react-router-dom';
+import { Select, Space } from 'antd';
 
+
+const { Option } = Select;
 interface Countries {
 
     name: {
@@ -49,6 +52,26 @@ function AllCountries() {
                     id='search'
                     placeholder='Search Country' />
             </form>
+            <Space wrap>
+                <Select className="mt-6"
+                    value={region}
+                    style={{ width: 120 }}
+                   onChange={(newRegion) => setRegion(newRegion)}
+                  
+                >
+
+                    <Option key="All" value="">
+                        All orders
+                    </Option>
+                    <Option key="Asia" value="Asia"> Asia</Option>
+                    <Option key="Africa" value="Africa"> Africa</Option>
+                    <Option key="Americas" value="Americas"> Americas</Option>
+                    <Option key="Antarctic" value="Antarctic"> Antarctic</Option>
+                    <Option key="Europe" value="Europe"> Europe</Option>
+                    <Option key="Oceania" value='Oceania'>Oceania</Option>
+
+                </Select>
+            </Space>
             <div>
                 {countriesData && countriesData.map((country) => (
                     <Link to={`/${country.name.common}`} key={country.name.official}>
